e28a655b7d134cb281a73c5af87bf9bd90d6aa44
K Annabel Bligh <kabligh@gmail.com>
2020-12-03 15:58:18 +0000

    Added current temperature in London and what it feels like

--- src/interface.js
+++ src/interface.js
@@ -33,4 +33,13 @@ $(document).ready(function() {
     $('#temperature').text(thermostat.temperature);
     $('#temperature').attr('class', thermostat.energyUsage());
   }
+
+  $.get('http://api.openweathermap.org/data/2.5/weather?q=London&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+  $('#current-temperature').text(data.main.temp);
+  })
+
+  $.get('http://api.openweathermap.org/data/2.5/weather?q=London&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+  $('#feels').text(data.main.feels_like);
+  })
+
 })
