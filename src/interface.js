2876a5c54bf755158b548cb1c9745bc33cd2055f
K Annabel Bligh <kabligh@gmail.com>
2020-12-03 16:11:07 +0000

    User can select cities from drop down menu

--- src/interface.js
+++ src/interface.js
@@ -42,4 +42,12 @@ $(document).ready(function() {
   $('#feels').text(data.main.feels_like);
   })
 
+  $('#current-city').change(function() {
+    var city = $('#current-city').val();
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+      $('#city-temperature').text(data.main.temp)
+      $('#feels').text(data.main.feels_like);
+    })
+  })
+
 })
